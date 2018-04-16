5cd45f2eb1da9234691872a2089ab1217a6c81b9
Esha Datta <eshadatta16@gmail.com>
2018-04-16 14:37:56 -0400

    removed email method

--- run_aspace_do.rb
+++ run_aspace_do.rb
@@ -1,31 +1,23 @@
 require 'open3'
 
-def email_process
-  subject = "ArchivesSpace Update of digital object uris"
-  msg = %Q{Hello,
-           This is an automated message notifying you that digital object uris for #{COLLECTION}
-           will be updated in Archives Space.
-           
-           If you have any concerns, please do not reply to this message. Contact: #{RESPONSE}}
-  cmd = "mail -v -s '#{subject}' #{EMAIL} <<< '#{msg}'"
-  stdout,err,status = Open3.capture3(cmd)
-  if err.empty?
-    puts "emailed #{EMAIL}"
-  else
-   puts "ERROR: #{err}"
-  end
+def chk_usage(usage)
+  usage = allowed.include?(usage)? usage : nil
+end
+
+def allowed
+  %w(audio-service video-service image-service image-thumbnail)
 end
 file = ARGV[0]
-EMAIL = ARGV[1]
-COLLECTION = ARGV[2]
-path = ARGV[3] || "/content/prod/rstar/content/fales/mss055/wip/se"
-RESPONSE = "ed64@nyu.edu"
-email_process
+usage = ARGV[1]
+path = ARGV[2] || "/content/prod/rstar/content/fales/mss460/wip/se" 
+if usage.nil?
+ raise "Illegal argument. Allowed values are allowed.join(" ")"
+end 
 do_uri_fname = "archival_object_uri"
 handle_fname = "handle" 
-APP_LOCATION="aspace-do-update/bin"
+APP_LOCATION="/home/dlib/datta/aspace-do-update/bin"
 updater_script = "aspace-do-update"
-usage = "image-service"
+usage = chk_usage(usage)
 handle_prefix = "http://hdl.handle.net/"
 entries = []
 if File.exist?(file)
@@ -44,7 +36,6 @@ entries.each { |e|
   key = File.basename(e)
   do_info[key] = [do_uri,handle_prefix+handle]
 }
-
 do_info.each_pair { |k,v|
   puts "#{k}: #{v}"
   do_uri = v[0]
