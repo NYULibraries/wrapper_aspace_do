9a57e65371e80f0a55e59d7aabb7e4b199607a82
jgpawletko <jgpawletko@gmail.com>
2022-07-18 11:14:54 -0400

    finish test refactoring

--- test/run_aspace_do_test.rb
+++ test/run_aspace_do_test.rb
@@ -24,15 +24,15 @@ class RunAspaceDo < MiniTest::Test
     o_array = o.split("\n")
     expected = 'bar: ["bar-uri", "http://hdl.handle.net/bar-handle"]' 
     actual   = o_array[0]
-    assert( expected == actual, "unexpected handle output: expected: '#{expected}', actual: '#{actual}'")
+    assert(expected == actual, "unexpected handle output: expected: '#{expected}', actual: '#{actual}'")
 
     expected = "#{env['RUN_ASPACE_DO_UPDATER_PATH']} -a bar-uri -f http://hdl.handle.net/bar-handle -u audio-service"
     actual   = o_array[1]
-    assert( expected == actual, "unexpected command string: expected: '#{expected}', actual: '#{actual}'")
+    assert(expected == actual, "unexpected command string: expected: '#{expected}', actual: '#{actual}'")
 
     expected = 'aspace-do-uri: success!'
     actual   = o_array[2]
-    assert( expected == actual, "unexpected result: expected: '#{expected}', actual: '#{actual}'")
+    assert(expected == actual, "unexpected result: expected: '#{expected}', actual: '#{actual}'")
 
     expected = 'foo: ["foo-uri", "http://hdl.handle.net/foo-handle"]'
     actual   = o_array[3]
@@ -40,11 +40,12 @@ class RunAspaceDo < MiniTest::Test
 
     expected = "#{env['RUN_ASPACE_DO_UPDATER_PATH']} -a foo-uri -f http://hdl.handle.net/foo-handle -u audio-service"
     actual   = o_array[4]
-    assert( expected == actual, "unexpected command string: expected: '#{expected}', actual: '#{actual}'")
+    assert(expected == actual, "unexpected command string: expected: '#{expected}', actual: '#{actual}'")
 
     expected = 'aspace-do-uri: success!'
     actual   = o_array[5]
-    assert( expected == actual, "unexpected result: expected: '#{expected}', actual: '#{actual}'")
+    assert(expected == actual, "unexpected result: expected: '#{expected}', actual: '#{actual}'")
+
     assert(e == '', "unexpected error output: expected '', actual: '#{e}'")
   end
 
@@ -52,13 +53,32 @@ class RunAspaceDo < MiniTest::Test
     env = {'RUN_ASPACE_DO_UPDATER_PATH' => 'test/mock_scripts/good-aspace-do-update'}
     o, e, s = Open3.capture3(env, "#{COMMAND} #{OK_WIP_LIST_FILE} 'image-thumbnail' #{OK_WIP_PATH}")
     assert(s.exitstatus == 0, "exit status: #{s.exitstatus}")
+
     o_array = o.split("\n")
-    assert('bar: ["bar-uri", "http://hdl.handle.net/bar-handle?urlappend=/mode/thumb"]' == o_array[0])
-    assert("#{env['RUN_ASPACE_DO_UPDATER_PATH']} -a bar-uri -f http://hdl.handle.net/bar-handle?urlappend=/mode/thumb -u image-thumbnail" == o_array[1])
-    assert('aspace-do-uri: success!' == o_array[2])
-    assert('foo: ["foo-uri", "http://hdl.handle.net/foo-handle?urlappend=/mode/thumb"]' == o_array[3])
-    assert("#{env['RUN_ASPACE_DO_UPDATER_PATH']} -a foo-uri -f http://hdl.handle.net/foo-handle?urlappend=/mode/thumb -u image-thumbnail" == o_array[4])
-    assert('aspace-do-uri: success!' == o_array[5])
-    assert(e == '')
+    expected = 'bar: ["bar-uri", "http://hdl.handle.net/bar-handle?urlappend=/mode/thumb"]'
+    actual   = o_array[0]
+    assert(expected == actual, "unexpected handle output: expected: '#{expected}', actual: '#{actual}'")
+
+    expected = "#{env['RUN_ASPACE_DO_UPDATER_PATH']} -a bar-uri -f http://hdl.handle.net/bar-handle?urlappend=/mode/thumb -u image-thumbnail"
+    actual   = o_array[1]
+    assert(expected == actual, "unexpected command string: expected: '#{expected}', actual: '#{actual}'")
+
+    expected = 'aspace-do-uri: success!'
+    actual   = o_array[2]
+    assert(expected == actual, "unexpected result: expected: '#{expected}', actual: '#{actual}'")
+
+    expected = 'foo: ["foo-uri", "http://hdl.handle.net/foo-handle?urlappend=/mode/thumb"]' 
+    actual   = o_array[3]
+    assert(expected == actual, "unexpected handle output: expected: '#{expected}', actual: '#{actual}'")
+
+    expected = "#{env['RUN_ASPACE_DO_UPDATER_PATH']} -a foo-uri -f http://hdl.handle.net/foo-handle?urlappend=/mode/thumb -u image-thumbnail" 
+    actual   = o_array[4]
+    assert(expected == actual, "unexpected command string: expected: '#{expected}', actual: '#{actual}'")
+
+    expected = 'aspace-do-uri: success!'
+    actual   = o_array[5]
+    assert(expected == actual, "unexpected result: expected: '#{expected}', actual: '#{actual}'")
+
+    assert(e == '', "unexpected error output: expected '', actual: '#{e}'")
   end
 end
